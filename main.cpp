5e317751ec8fb93e7b4a8aef1ff7b5a7479db56f
Vishal2186 <vishal.609.vishal.609@gmail.com>
2025-02-06 19:12:15 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+int n = 200;
+
+void animate() {
+    const int width = 100; // Width of the console animation
+    int pos = 0;
+    int direction = 1; // 1 for right, -1 for left
+
+    while (n--) {
+        system("clear");
+    cout<<"\n\nSHIBUYA STATION                                                                                             TOKYO STATION";
+        cout<<endl;cout<<endl;
+        // Print spaces before the dot
+        for (int i = 0; i < pos; i++) {
+            cout << " ";
+        }
+        cout << "<--|(o_o)|__|(o_o)|__|(o_o)|-->" <<endl;
+
+        // Update position
+        pos += direction;
+        if (pos == width || pos == 0) {
+            direction *= -1; // Reverse direction
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(100)); // Adjust speed
+    }
+}
+
+int main() {
+    animate();
+    cout<<endl;
+    cout<<endl;
+    cout<<"\t\t\t\t\t\t\tH A P P Y   J O U R N Y";
+    cout<<endl;
+    cout<<endl;
+    return 0;
+}
